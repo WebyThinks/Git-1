c15c4cbe02952c897761ee398eb96aac966bbd5c
Michael Cavender <michaelc@wowbrands.com>
2013-09-30 09:37:38 -0400

    Start on GitRepo class

---
+++ src/MWCavender/Git/GitRepo.php
@@ -0,0 +1,32 @@
+<?php
+
+use MWCavender\Common\Executer;
+
+class GitRepo extends Executer
+{
+    public function __construct()
+    {
+        $this->setApplication('git');
+    }
+
+    public static function checkout($branch)
+    {
+        $instance = (new static)->addArgument('checkout')->addArgument($branch);
+
+        return $instance;
+    }
+
+    public static function clone($repo)
+    {
+        $instance = (new static)->addArgument('clone')->addArgument($repo);
+
+        return $instance;
+    }
+
+    public function into($path)
+    {
+        $this->addArgument(realpath($path));
+
+        return $this;
+    }
+}
